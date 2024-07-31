deb8e5243dd4b26555c814a0d3d4d5623979cded
Tharan <tharan0026@gmail.com>
2024-07-31 10:47:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,40 @@
-function add(){
-    const typ=document.getElementById("type").value;
-    const name=document.getElementById("name").value;
-    const amt=document.getElementById("amnt").value;
-    const data=document.getElementById("dt");
+function add() {
+    const typ = document.getElementById("type");
+    const name = document.getElementById("name");
+    const amt = document.getElementById("amnt");
+    const data = document.getElementById("dt");
 
-    if(typ===""){
+    const typValue = typ.value;
+    const nameValue = name.value;
+    const amtValue = amt.value;
+
+    if (typValue === "") {
         alert("Please select a type");
         return;
-    }
-    else if(name==="")
-    {
+    } else if (nameValue === "") {
         alert("Enter a name");
         return;
-    }
-    else if(amt===""){
+    } else if (amtValue === "") {
         alert("Enter an amount");
         return;
     }
 
-    const trow=document.createElement("tr");
-    const ttype=document.createElement("td");
-    const tname=document.createElement("td");
-    const tamt=document.createElement("td");
-    const tdelete=document.createElement("td");
+    const trow = document.createElement("tr");
+    const ttype = document.createElement("td");
+    const tname = document.createElement("td");
+    const tamt = document.createElement("td");
+    const tdelete = document.createElement("td");
 
-    const deleteBtn=document.createElement("button");
-    deleteBtn.textContent="Delete";
+    const deleteBtn = document.createElement("a");
+    deleteBtn.textContent = "Delete";
 
-    deleteBtn.onclick=function() {
+    deleteBtn.onclick = function() {
         trow.remove();
     };
-    ttype.textContent = typ;
-    tname.textContent = name;
-    tamt.textContent = amt;
+
+    ttype.textContent = typValue;
+    tname.textContent = nameValue;
+    tamt.textContent = amtValue;
     tdelete.appendChild(deleteBtn);
     trow.appendChild(ttype);
     trow.appendChild(tname);
@@ -40,6 +42,9 @@ function add(){
     trow.appendChild(tdelete);
     data.appendChild(trow);
 
+    typ.value = "";
+    name.value = "";
+    amt.value = "";
+
     return false;
-    
 }
